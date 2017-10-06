128ca713785f0e508e9a04cbe988524a70f6fe98
Christopher Diep <diep.christopher@gmail.com>
2017-10-06 00:25:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,10 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  array = array.unshift(element)
+  return array.unshift(element)
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  var array = array.unshift(element)
   return array
 }
