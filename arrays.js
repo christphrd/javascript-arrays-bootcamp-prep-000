c62cb9505c209388ce23b7dde620be0c1c22214b
Christopher Diep <diep.christopher@gmail.com>
2017-10-06 00:48:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  array.unshift(element)
+  array = array.unshift(element)
 }
 
 function addElementToEndOfArray(array,element) {
@@ -13,5 +13,5 @@ function addElementToEndOfArray(array,element) {
 }
 
 function destructivelyAddElementToEndOfArray(array,element) {
-  array.push(element)
+  array = array.push(element)
 }
